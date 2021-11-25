2f66c9276a3efb1cc22be7f9e491e27a9f325b0e
Alexey Zapromyotov <ZAAL6006@MUHCAD.MUHCFRD.CA>
2021-11-25 18:36:28 -0500

    commit 2

---
+++ lib/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBNAME', 'quizdb');
+// define('DBHOST', '172.26.125.158');
+// define('DBUSER', 'quizad');
+// define('DBPASS', 'Pt468YSJWXnsgGEm!');
+// define('DBNAME', 'quizdb');
+//
+?>
